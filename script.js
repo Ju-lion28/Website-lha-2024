d3083b5f108de833b5424c4b84db9c3046b94112
Julian Sarli <sarlij485@gmail.com>
2023-10-06 18:38:26 -0400

    dark mode light mode

--- script.js
+++ script.js
@@ -6,64 +6,36 @@ var frenchNav = document.getElementById("frenchNav");
 
 // ty gpt my love
 document.addEventListener('DOMContentLoaded', function() {
-  var menuLinks = document.querySelectorAll('#picker a');
+	var menuLinks = document.querySelectorAll('#picker a');
 
-  menuLinks.forEach(function(link) {
-    link.addEventListener('click', function(e) {
-      var hash = this.hash;
-      var targetSection = document.querySelector(hash);
+	menuLinks.forEach(function(link) {
+		link.addEventListener('click', function(e) {
+			var hash = this.hash;
+			var targetSection = document.querySelector(hash);
 
-      if (targetSection) {
-        e.preventDefault();
+			if (targetSection) {
+				e.preventDefault();
 
-        var topOffset = targetSection.offsetTop;
-        var scrollOptions = {
-          top: topOffset,
-          behavior: 'smooth'
-        };
+				var topOffset = targetSection.offsetTop;
+				var scrollOptions = {
+					top: topOffset,
+					behavior: 'smooth'
+				};
 
-        window.scrollTo(scrollOptions);
-      }
-    });
-  });
+				window.scrollTo(scrollOptions);
+			}
+		});
+	});
 });
 // end of gpt
 
-/////////////////////////////////////////////////////////////////////////////////
+const lightThemeButton = document.getElementById('light-theme');
+const darkThemeButton = document.getElementById('dark-theme');
 
-// document.onload = function() {
-//   changeScore()
-// }
+lightThemeButton.addEventListener('click', () => {
+  document.documentElement.setAttribute('data-theme', 'light');
+});
 
-// function changeScore(value) {
-//   let highScore = localStorage.getItem("highScore");
-
-//   console.log(highScore)
-
-//   if (value === null || NaN) {
-//     console.log(highScore);
-//     document.getElementById("highscoreElement").innerText = highScore.toString();
-//   } else {
-//     localStorage.setItem("highScore", Number(highScore) + value);
-
-//     console.log(highScore);
-
-//     document.getElementById("highscoreElement").innerText = highScore.toString();
-//   }
-
-//   console.log(highScore)
-
-// }
-
-// let aElements = document.getElementsByTagName('a');
-// for (let i = 0; i < aElements.length; i++) {
-//   aElements[i].addEventListener("click", function() {
-//     changeScore(10)
-//   });
-// }
-
-// document.addEventListener("click", function() {
-//   changeScore(1)
-// });
-
-
+darkThemeButton.addEventListener('click', () => {
+  document.documentElement.setAttribute('data-theme', 'dark');
+});
