541df5642a9b02d583fd51b8efda6c2950e1d426
Julian Sarli <sarlij485@gmail.com>
2023-10-08 17:54:41 -0400

    added transition and broke themes - the transition is still dumb its just a placeholder for now - i had made it so that the theme would be set in localstorage for coolness but um i broke it with the transition

--- script.js
+++ script.js
@@ -1,42 +1,59 @@
-var journalNav = document.getElementById("journalNav");
-var crcNav = document.getElementById("crcNav");
-var gameNav = document.getElementById("gameNav");
-var rosterNav = document.getElementById("rosterNav");
-var frenchNav = document.getElementById("frenchNav");
+// Function to load a new page and handle transitions
+const loadPage = (url) => {
+	// Apply a transition effect (e.g., fading) before loading new content
+	document.body.style.opacity = 0;
 
-document.documentElement.setAttribute('data-theme', 'light');
-const lightThemeButton = document.getElementById('light-theme');
-const darkThemeButton = document.getElementById('dark-theme');
+	// Fetch the new page content
+	fetch(url)
+		.then(response => response.text())
+		.then(data => {
+			// Replace the content of the entire body with the new page's content
+			document.body.innerHTML = data;
 
-lightThemeButton.addEventListener('click', () => {
-	document.documentElement.setAttribute('data-theme', 'light');
-});
-
-darkThemeButton.addEventListener('click', () => {
-	document.documentElement.setAttribute('data-theme', 'dark');
-});
-
-// ty gpt my love
-document.addEventListener('DOMContentLoaded', function() {
-	var menuLinks = document.querySelectorAll('#picker a');
-
-	menuLinks.forEach(function(link) {
-		link.addEventListener('click', function(e) {
-			var hash = this.hash;
-			var targetSection = document.querySelector(hash);
-			
-			if (targetSection) {
-				e.preventDefault();
-				
-				var topOffset = targetSection.offsetTop;
-				var scrollOptions = {
-					top: topOffset,
-					behavior: 'smooth'
-				};
-
-				window.scrollTo(scrollOptions);
-			}
+			// Remove the transition effect after loading new content
+			setTimeout(() => {
+				document.body.style.opacity = 1;
+			}, 1000); // Adjust the timeout duration as needed
 		});
+};
+
+// Event listeners for navigation links (including images inside anchor tags)
+document.addEventListener('click', (event) => {
+	const target = event.target;
+
+	// Check if the clicked element is a navigation link or a child element of a link
+	if (target.tagName === 'A' || target.closest('a')) {
+		event.preventDefault(); // Prevent the default link behavior
+
+		// Get the URL of the clicked link
+		const url = (target.tagName === 'A') ? target.getAttribute('href') : target.closest('a').getAttribute('href');
+  
+		// Load the new page
+		loadPage(url);
+	}
+});
+
+// Check if there is a stored theme preference, and if not, set the default theme to "light"
+const storedTheme = localStorage.getItem('theme');
+if (storedTheme) {
+	document.documentElement.setAttribute('data-theme', storedTheme);
+} else {
+	document.documentElement.setAttribute('data-theme', 'light');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+	const lightThemeButton = document.getElementById('light-theme');
+	const darkThemeButton = document.getElementById('dark-theme');
+	console.log(lightThemeButton)
+	console.log(darkThemeButton)
+
+	lightThemeButton.addEventListener('click', () => {
+		document.documentElement.setAttribute('data-theme', 'light');
+		localStorage.setItem('theme', 'light'); // Store the theme preference
+	});
+
+	darkThemeButton.addEventListener('click', () => {
+		document.documentElement.setAttribute('data-theme', 'dark');
+		localStorage.setItem('theme', 'dark'); // Store the theme preference
 	});
 });
-// end of gpt
