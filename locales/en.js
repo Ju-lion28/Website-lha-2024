157ddcbcbb2934b856e2ef660ef024fed9a6a0c5
Abadima <abadima06@gmail.com>
2023-11-13 19:11:18 -0500

    Goodnight (read description for logs)
    - Added translations for home page
    - Fixed up "some" CSS code
    - Footer now is where it needs to be
    - Fixed broken image links
    - Remove "ENGLISH" / "FRENCH" setting as it's now determined automatically based on browser language

---
+++ locales/en.js
@@ -0,0 +1,16 @@
+const en = {
+    name: "English",
+    home: {
+        menu_options: {
+            game: "Game",
+            journal: "Journal",
+            roster: "Roster"
+        },
+        about_us: {
+            title: "About Us",
+            text: "We are a group of students in LaurenHill Academy's Robotics program. Together, we have built innovative machinery, videogames, and even this website! LHA students are always welcome to join our team."
+        }
+    }
+};
+
+window.localization = en;
---
+++ locales/fr.js
@@ -0,0 +1,16 @@
+const fr = {
+    name: "Français",
+    home: {
+        menu_options: {
+            game: "Jeu",
+            journal: "Journal",
+            roster: "Roster"
+        },
+        about_us: {
+            title: "À propos de nous",
+            text: "Nous sommes un groupe d'élèves du programme de robotique de la LaurenHill Academy. Ensemble, nous avons construit des machines innovantes, des jeux vidéo et même ce site web! Les élèves de la LHA sont toujours les bienvenus dans notre équipe."
+        }
+    }
+};
+
+window.localization = fr;
