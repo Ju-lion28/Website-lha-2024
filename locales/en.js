4c9e7911078d7eb37af15a6a9ea6d47a5a2d6594
Abadima <abadima06@gmail.com>
2023-11-16 10:31:21 -0500

    This took way too long to finish oml
    - Fix initial translation issues
    - Added Spanish
    - Updated CSS for cooler design
    - Added Language Toggle Feature
    - Fix Theme Toggle Feature (yw Julian)

--- locales/en.js
+++ locales/en.js
@@ -4,12 +4,14 @@ const en = {
         menu_options: {
             game: "Game",
             journal: "Journal",
+            language: "Language",
             roster: "Roster"
         },
         about_us: {
             title: "About Us",
             text: "We are a group of students in LaurenHill Academy's Robotics program. Together, we have built innovative machinery, videogames, and even this website! LHA students are always welcome to join our team."
-        }
+        },
+        title: "LHA Robotics 2024"
     }
 };
 
--- locales/fr.js
+++ locales/fr.js
@@ -4,12 +4,14 @@ const fr = {
         menu_options: {
             game: "Jeu",
             journal: "Journal",
+            language: "Langue",
             roster: "Roster"
         },
         about_us: {
             title: "À propos de nous",
             text: "Nous sommes un groupe d'élèves du programme de robotique de la LaurenHill Academy. Ensemble, nous avons construit des machines innovantes, des jeux vidéo et même ce site web! Les élèves de la LHA sont toujours les bienvenus dans notre équipe."
-        }
+        },
+        title: "LHA Robotique 2024"
     }
 };
 
