fd7a3a9c95e20983d3d526389532183b9b8ddf97
losanni <58642077+losanni@users.noreply.github.com>
2024-03-07 15:52:39 -0500

    First build scripts
    - Changes to picture names

--- README.md
+++ README.md
@@ -12,3 +12,4 @@
 npm install .
 ```
 
+### Build script
---
+++ build.sh
@@ -0,0 +1,15 @@
+rm -rf .parcel-cache
+
+rm -rf dist
+
+npx parcel build index.html 
+
+mkdir dist/locales
+
+cp locales/* dist/locales
+
+mkdir dist/assets/
+mkdir dist/assets/icons
+mkdir dist/assets/icons/theme
+
+cp assets/icons/theme/* dist/assets/icons/theme
