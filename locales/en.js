a34816252453e7ea7b4a87cee57468a337dd1440
Abadi <abadima06@gmail.com>
2024-03-07 20:28:12 -0500

    Translation File Updates

--- locales/en.js
+++ locales/en.js
@@ -32,6 +32,17 @@ const en = {
 		title: "LHA Robotics 2024 Roster"
 	},
 	journal: {
+		Kiosk: {
+			dec: "After rebuilding the kiosk, we began sketching. We completed half of it before the break. We also planned the colors to be used.",
+			jan: "We finished the sketch during this month. However, we couldn't start painting immediately because we lacked paint. Eventually, we painted the pink land and the base of the sky.",
+			feb: "In February, we painted mountains and lakes, which were made out of chocolate, and added intricate details to them."
+		},
+		Media: {
+			aug: "In August we wrote the video script. The script was reviewed and rewritten multiple times with different plots to see what would work best within the canon of the original Wreck-It-Ralph story. Progress was delayed until we could find a costume for George.",
+			jan: "In January we received the costumes.",
+			feb: "Filming went smoothly. On Monday, Samir, George and Uzma started to film part 2 of the movie since Uzma had to leave for Bangladesh. We had a lot of fun filming this part of the movie. We had to do a lot of retakes because we were laughing a lot and we had people from the school interrupting us. In the last week of February Samir and George went all around Montreal filming in different places. We went to Saint Joseph's Oratory of Mount Royal, the tip of the Montreal island and filmed on the water. We took many different shots that will make the video look cool. We 3D printed props for the movie and we started filming the last parts of the movie and the editing started.",
+			mar: "In March the team finished the video. We had to put together the video and edit the footage we had to make it all come together. We also got final shots of our main character, Ralph, played by George, working on our robot. It was actually quite challenging, with the dead-line looming over us."
+		},
 		months: {
 			jan: "January",
 			feb: "February",
@@ -45,6 +56,20 @@ const en = {
 			oct: "October",
 			nov: "November",
 			dec: "December"
+		},
+		Programming: {
+			aug: "In August, significant updates were made to the website. We transitioned to an arcade-style theme, complete with a CRT screen effect, and introduced a color scheme predominantly comprised of black and blue. The addition of a pixelated font enhanced the retro aesthetic. Practical improvements included the incorporation of a readme file and adjustments to the font selection. Additionally, a .gitignore file was implemented to streamline version control processes.",
+			oct: "In October, our efforts focused on enhancing the website's organization and visual appeal. We implemented a systematic approach, ensuring that every page served a distinct purpose. Transitioning from the previous arcade theme, we embraced a more vibrant candy aesthetic, unifying the color and theme across all pages for a cohesive experience. We added dark/light modes because dark mode is awesome. Adding a fade-out transition between pages elevated the overall user experience, providing a seamless browsing journey. Additionally, we dedicated time to polishing existing features, refining elements such as headers, theme switchers, transitions, and image spacing and sizing. Yanni introduced a scoring feature, awarding 2 points per click and 10 points per link, enhancing user engagement. However, the theme switcher encountered issues, requiring extensive troubleshooting for the remainder of the month. Despite this setback, our commitment to quality and functionality remained unwavering as we worked diligently to resolve the issue and ensure a smooth user experience.",
+			nov: "In November, our focus shifted towards refining the website's functionality and appearance. Addressing formatting issues, we improved the overall aesthetic appeal. Translation problems were resolved, including the removal of duplicate files associated with the French version. We also implemented automatic language detection based on system preferences. Theme switcher issues were rectified, enhancing user experience consistency. Julian recommended adding manual language change options, providing users with more control. Additionally, we streamlined the homepage and extended its features to other pages for uniformity. This involved adjustments to headers, theme switchers, and language options across the site. To optimize mobile browsing, we introduced a hamburger menu for smaller screens, ensuring ease of navigation on phones. These updates reflect our commitment to delivering a seamless and user-friendly website experience.",
+			dec: "In December, our focus shifted towards setting up the roster section of the website. We introduced boxes displaying both people's names and faces, although descriptions were not yet included. Initially attempting to make the boxes clickable for additional information, we encountered difficulties, prompting us to make them hoverable instead. Despite these efforts, the functionality remained peculiar, and challenges persisted in achieving the desired interaction. We acknowledge the current state of the roster section and continue to explore solutions for improvement."
+		},
+		Robot: {
+			sep: "We tried out different wheels and bases to find the best fit for our pilot. This took some time since we had to think about whether we wanted ease of use or degrees of motion.",
+			oct: "We decided to use last year’s base for the robot. Since this is our second year, knowing what we know now, we were able to make the right changes quicker and we knew what to prioritize and what to discard.",
+			nov: "Kickoff!!! The Kickoff was great and we excitedly began planning and brainstorming ideas. After the initial brainstorming was complete we ordered parts and prepared for assembly.",
+			dec: "In December is when we started building. The first prototypes we made were rough but after a bit of trial and error we got to a good design for the base.",
+			jan: "This is when we started work on the ball pick-up mechanisms. We went through three prototypes before finally landing on the right one. In the meantime we were also working on shooting mechanisms.",
+			feb: "As well as some finishing touches, everything is now assembled and (almost) ready to go. All we have to do now is fix up our wiring."
 		}
 	}
 };
--- locales/fr.js
+++ locales/fr.js
@@ -34,6 +34,17 @@ const fr = {
 		title: "LHA Robotics 2024 Roster"
 	},
 	journal: {
+		Kiosk: {
+			dec: "Après avoir reconstruit le kiosque, nous avons commencé à dessiner. Nous en avons réalisé la moitié avant la pause. Nous avons également planifié les couleurs à utiliser.",
+			jan: "Nous avons terminé l'esquisse au cours de ce mois. Cependant, nous n'avons pas pu commencer à peindre immédiatement car nous manquions de peinture. Finalement, nous avons peint la terre rose et la base du ciel.",
+			feb: "En février, nous avons peint des montagnes et des lacs en chocolat et y avons ajouté des détails complexes."
+		},
+		Media: {
+			aug: "En août, nous avons écrit le scénario de la vidéo. Le scénario a été revu et réécrit plusieurs fois avec différentes intrigues pour voir ce qui fonctionnerait le mieux dans le canon de l'histoire originale de Wreck-It-Ralph. L'avancement du projet a été retardé jusqu'à ce que nous trouvions un costume pour George.",
+			jan: "En janvier, nous avons reçu les costumes.",
+			feb: "Le tournage s'est déroulé sans encombre. Lundi, Samir, George et Uzma ont commencé à filmer la deuxième partie du film, Uzma ayant dû partir pour le Bangladesh. Nous nous sommes beaucoup amusés à filmer cette partie du film. Nous avons dû faire beaucoup de reprises parce que nous riions beaucoup et que des gens de l'école nous interrompaient. Au cours de la dernière semaine de février, Samir et George sont allés filmer dans différents endroits de Montréal. Nous sommes allés à l'Oratoire Saint-Joseph du Mont-Royal, à l'extrémité de l'île de Montréal et nous avons filmé sur l'eau. Nous avons pris beaucoup de photos différentes qui donneront un aspect cool à la vidéo. Nous avons imprimé en 3D des accessoires pour le film et nous avons commencé à filmer les dernières parties du film et le montage a commencé.",
+			mar: "En mars, l'équipe a terminé la vidéo. Nous avons dû monter la vidéo et éditer les séquences dont nous disposions pour que le tout soit cohérent. Nous avons également réalisé les dernières prises de vue de notre personnage principal, Ralph, joué par George, travaillant sur notre robot. C'était en fait un véritable défi, avec la date butoir qui se profilait à l'horizon."
+		},
 		months: {
 			jan: "Janvier",
 			feb: "Février",
@@ -47,6 +58,18 @@ const fr = {
 			oct: "Octobre",
 			nov: "Novembre",
 			dec: "Décembre"
+		},
+		Programming: {
+			aug: "En août, des mises à jour importantes ont été apportées au site web. Nous avons opté pour un thème de style arcade, avec un effet d'écran CRT, et introduit une palette de couleurs composée principalement de noir et de bleu. L'ajout d'une police de caractères pixelisée a renforcé l'esthétique rétro. Les améliorations pratiques comprennent l'incorporation d'un fichier readme et des ajustements à la sélection de la police. En outre, un fichier .gitignore a été mis en place pour rationaliser les processus de contrôle des versions.",
+			oct: "En octobre, nos efforts se sont concentrés sur l'amélioration de l'organisation et de l'attrait visuel du site web. Nous avons mis en œuvre une approche systématique, en veillant à ce que chaque page ait un objectif distinct. Nous avons abandonné le thème de l'arcade pour adopter une esthétique plus vibrante, unifiant les couleurs et le thème sur toutes les pages pour une expérience cohérente. Nous avons ajouté des modes sombre et clair parce que le mode sombre est génial. L'ajout d'une transition en fondu entre les pages a amélioré l'expérience globale de l'utilisateur, en lui offrant un parcours de navigation fluide. En outre, nous avons consacré du temps à l'amélioration des fonctionnalités existantes, en peaufinant des éléments tels que les en-têtes, les commutateurs de thèmes, les transitions, ainsi que l'espacement et la taille des images. Yanni a introduit une fonction de notation, attribuant 2 points par clic et 10 points par lien, améliorant ainsi l'engagement de l'utilisateur. Cependant, le sélecteur de thème a rencontré des problèmes qui ont nécessité un dépannage approfondi pendant le reste du mois. Malgré ce contretemps, notre engagement envers la qualité et la fonctionnalité est resté inébranlable et nous avons travaillé avec diligence pour résoudre le problème et garantir une expérience utilisateur fluide.",
+			nov: "En novembre, nous nous sommes concentrés sur l'amélioration de la fonctionnalité et de l'apparence du site web. En réglant les problèmes de formatage, nous avons amélioré l'attrait esthétique général. Les problèmes de traduction ont été résolus, y compris la suppression des fichiers en double associés à la version française. Nous avons également mis en œuvre la détection automatique de la langue en fonction des préférences du système. Les problèmes de changement de thème ont été corrigés, ce qui a permis d'améliorer la cohérence de l'expérience utilisateur. Julian a recommandé d'ajouter des options manuelles de changement de langue, ce qui permet aux utilisateurs d'avoir un meilleur contrôle. En outre, nous avons rationalisé la page d'accueil et étendu ses fonctionnalités à d'autres pages dans un souci d'uniformité. Pour ce faire, nous avons ajusté les en-têtes, les sélecteurs de thèmes et les options linguistiques sur l'ensemble du site. Pour optimiser la navigation mobile, nous avons introduit un menu hamburger pour les petits écrans, afin de faciliter la navigation sur les téléphones. Ces mises à jour reflètent notre engagement à fournir une expérience de site web transparente et conviviale.",
+			dec: "En décembre, nous nous sommes concentrés sur la mise en place de la section 'liste' du site web. Nous avons introduit des cases affichant les noms et les visages des personnes, bien que les descriptions ne soient pas encore incluses. Nous avons d'abord essayé de rendre les cases cliquables pour obtenir des informations supplémentaires, mais nous avons rencontré des difficultés, ce qui nous a incités à les rendre survolables à la place. Malgré ces efforts, la fonctionnalité est restée particulière et il a été difficile d'obtenir l'interaction souhaitée. Nous prenons acte de l'état actuel de la section du tableau de service et continuons à explorer des solutions pour l'améliorer."
+		},
+		Robot: {
+			sept: "Nous avons essayé différentes roues et bases pour trouver ce qui convenait le mieux à notre pilote. Cela nous a pris du temps, car nous devions nous demander si nous voulions une facilité d'utilisation ou des degrés de mouvement.",
+			oct: "Nous avons décidé d'utiliser la base de l'année dernière pour le robot. Comme c'est notre deuxième année, en sachant ce que nous savons maintenant, nous avons pu faire les bons changements plus rapidement et nous savions ce qu'il fallait prioriser et ce qu'il fallait écarter.",
+			nov: "Kickoff !!! Le kickoff a été formidable et nous avons commencé à planifier et à réfléchir à des idées avec enthousiasme. Une fois le brainstorming initial terminé, nous avons commandé des pièces et nous nous sommes préparés à l'assemblage.",
+			dec: "C'est en décembre que nous avons commencé à construire. Les premiers prototypes que nous avons réalisés étaient grossiers, mais après quelques essais et erreurs, nous sommes parvenus à une bonne conception de la base."
 		}
 	}
 };
