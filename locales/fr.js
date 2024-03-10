cf649d063a5f8ebf2dec81a1dcc0b949f6e52610
Abadi <abadima06@gmail.com>
2024-03-10 13:23:55 -0400

    Completed Translations
    - French for Journal
    - Fixed experience.html name

--- locales/fr.js
+++ locales/fr.js
@@ -15,23 +15,14 @@ const fr = {
 		language: "Langue",
 		roster: "Roster"
 	},
-	roster: {
-		leaders: "Meneurs",
-		roles: {
-			art: "L'Art Digital",
-			captain: "Capitaine",
-			kiosk: "Kiosque",
-			media: "Médias / Production",
-			pilot: "Pilote",
-			programming: "Programmation",
-			robot_building: "Construction de robots",
-			robot_design: "et Conception",
-			secondary: "Secondaire",
-			teacher: "Enseignant",
-			website: "Site Web"
-		},
-		team: "Équipe",
-		title: "LHA Robotics 2024 Roster"
+	experience: {
+		title1: "L'expérience des étudiants",
+		xpgallerylink: "Voir la Galerie",
+		stxp1: "La compétition CRC est vraiment l'une des plus grandes opportunités pour notre équipe. L'année dernière, c'était la première fois que nous revenions au CRC et, sans expérience, nous avons tout de même réussi à obtenir un excellent classement, à tisser des liens avec d'autres écoles et à acquérir davantage de connaissances sur ce qu'il faut préparer et comment se préparer pour l'année à venir. ",
+		stxp2: "L'année dernière, nous avons eu beaucoup de mal à nous organiser, alors cette année, nous en avons tenu compte. Nous avons créé des équipes et des sous-équipes afin d'être plus efficaces. Cependant, nous avons accepté trop de personnes dans l'équipe et nous n'avons pas pu suivre qui était où et qui faisait quoi. Il y a donc eu des conflits pour déterminer qui devait aller où en fonction de ses compétences, mais nous nous en sommes sortis ensemble. Aujourd'hui, nous avons réussi à stabiliser le flux et tout se passe bien. ",
+		stxp3: "L'une des nombreuses valeurs d'apprentissage a été la compréhension du rôle de leader. Nous avons tous coopéré pour déléguer des tâches et maintenir l'ordre dans nos équipes. ",
+		stxp4: "La partie la plus intéressante de l'équipe de robotique est qu'elle est principalement composée de diplômés, ce qui nous permet de créer de nombreux souvenirs inoubliables. La course aux collations dans la salle des robots, la vieille radio à moitié cassée qui tient compagnie à l'équipe du kiosque pendant qu'elle peint, et l'équipe de programmation qui passe 'le meilleur moment de sa vie' à déboguer son travail.",
+		stxp5: "De nombreux modèles de ballons ont été utilisés pour jouer à l'attrapeur agressif et de nombreux cheveux ont été arrachés à cause du stress. Sans oublier de mentionner les énormes poches sous les yeux de la plupart des gens. Beaucoup de temps a été sacrifié et de nombreuses heures d'étude se sont transformées en séances de construction de robots ou de peinture de kiosques, ce qui s'est traduit par de mauvaises notes aux examens. De plus, j'ai passé d'innombrables jours à l'extérieur avec nos amis, mais j'ai continué à travailler sur les éléments à soumettre PENDANT NOTRE PAUSE. Les larmes n'ont pas seulement coulé, elles ont jailli. "
 	},
 	journal: {
 		Kiosk: {
@@ -70,36 +61,48 @@ const fr = {
 			aug: "En août, des mises à jour importantes ont été apportées au site web. Nous avons opté pour un thème de style arcade, avec un effet d'écran CRT, et introduit une palette de couleurs composée principalement de noir et de bleu. L'ajout d'une police de caractères pixelisée a renforcé l'esthétique rétro. Les améliorations pratiques comprennent l'incorporation d'un fichier readme et des ajustements à la sélection de la police. En outre, un fichier .gitignore a été mis en place pour rationaliser les processus de contrôle des versions.",
 			oct: "En octobre, nos efforts se sont concentrés sur l'amélioration de l'organisation et de l'attrait visuel du site web. Nous avons mis en œuvre une approche systématique, en veillant à ce que chaque page ait un objectif distinct. Nous avons abandonné le thème de l'arcade pour adopter une esthétique plus vibrante, unifiant les couleurs et le thème sur toutes les pages pour une expérience cohérente. Nous avons ajouté des modes sombre et clair parce que le mode sombre est génial. L'ajout d'une transition en fondu entre les pages a amélioré l'expérience globale de l'utilisateur, en lui offrant un parcours de navigation fluide. En outre, nous avons consacré du temps à l'amélioration des fonctionnalités existantes, en peaufinant des éléments tels que les en-têtes, les commutateurs de thèmes, les transitions, ainsi que l'espacement et la taille des images. Yanni a introduit une fonction de notation, attribuant 2 points par clic et 10 points par lien, améliorant ainsi l'engagement de l'utilisateur. Cependant, le sélecteur de thème a rencontré des problèmes qui ont nécessité un dépannage approfondi pendant le reste du mois. Malgré ce contretemps, notre engagement envers la qualité et la fonctionnalité est resté inébranlable et nous avons travaillé avec diligence pour résoudre le problème et garantir une expérience utilisateur fluide.",
 			nov: "En novembre, nous nous sommes concentrés sur l'amélioration de la fonctionnalité et de l'apparence du site web. En réglant les problèmes de formatage, nous avons amélioré l'attrait esthétique général. Les problèmes de traduction ont été résolus, y compris la suppression des fichiers en double associés à la version française. Nous avons également mis en œuvre la détection automatique de la langue en fonction des préférences du système. Les problèmes de changement de thème ont été corrigés, ce qui a permis d'améliorer la cohérence de l'expérience utilisateur. Julian a recommandé d'ajouter des options manuelles de changement de langue, ce qui permet aux utilisateurs d'avoir un meilleur contrôle. En outre, nous avons rationalisé la page d'accueil et étendu ses fonctionnalités à d'autres pages dans un souci d'uniformité. Pour ce faire, nous avons ajusté les en-têtes, les sélecteurs de thèmes et les options linguistiques sur l'ensemble du site. Pour optimiser la navigation mobile, nous avons introduit un menu hamburger pour les petits écrans, afin de faciliter la navigation sur les téléphones. Ces mises à jour reflètent notre engagement à fournir une expérience de site web transparente et conviviale.",
-			dec: "En décembre, nous nous sommes concentrés sur la mise en place de la section 'liste' du site web. Nous avons introduit des cases affichant les noms et les visages des personnes, bien que les descriptions ne soient pas encore incluses. Nous avons d'abord essayé de rendre les cases cliquables pour obtenir des informations supplémentaires, mais nous avons rencontré des difficultés, ce qui nous a incités à les rendre survolables à la place. Malgré ces efforts, la fonctionnalité est restée particulière et il a été difficile d'obtenir l'interaction souhaitée. Nous prenons acte de l'état actuel de la section du tableau de service et continuons à explorer des solutions pour l'améliorer."
+			dec: "En décembre, nous nous sommes concentrés sur la mise en place de la section 'liste' du site web. Nous avons introduit des cases affichant les noms et les visages des personnes, bien que les descriptions ne soient pas encore incluses. Nous avons d'abord essayé de rendre les cases cliquables pour obtenir des informations supplémentaires, mais nous avons rencontré des difficultés, ce qui nous a incités à les rendre survolables à la place. Malgré ces efforts, la fonctionnalité est restée particulière et il a été difficile d'obtenir l'interaction souhaitée. Nous prenons acte de l'état actuel de la section du tableau de service et continuons à explorer des solutions pour l'améliorer.",
+			jan_feb: "En janvier et février, nous avons poursuivi le développement de l'expérience utilisateur sur le site web. Nous avons notamment corrigé de nombreux bugs mineurs. Nous avons également procédé à des ajustements des feuilles de style et de la mise à l'échelle du contenu du site web. De même, nous avons ajouté plus de membres de l'équipe à notre page de liste, en utilisant des icônes de remplacement pour certains d'entre eux. Enfin, nous avons ajouté plus d'informations sur la page du journal.",
+			mar: "En mars, nous avons effectué les derniers changements pour préparer notre site web à la publication. Comme toujours, de nombreuses corrections de bogues et modifications de la qualité de vie ont été apportées. Nous avons considérablement amélioré le journal avec beaucoup plus de détails, une barre de navigation latérale améliorée, des images et des vidéos. Enfin, nous avons optimisé le site web pour accélérer les temps de chargement et la diffusion des images."
 		},
 		Robot: {
 			sep: "Nous avons essayé différentes roues et bases pour trouver ce qui convenait le mieux à notre pilote. Cela nous a pris du temps, car nous devions nous demander si nous voulions une facilité d'utilisation ou des degrés de mouvement.",
 			oct: "Nous avons décidé d'utiliser la base de l'année dernière pour le robot. Comme c'est notre deuxième année, en sachant ce que nous savons maintenant, nous avons pu faire les bons changements plus rapidement et nous savions ce qu'il fallait prioriser et ce qu'il fallait écarter.",
 			nov: "Kickoff !!! Le kickoff a été formidable et nous avons commencé à planifier et à réfléchir à des idées avec enthousiasme. Une fois le brainstorming initial terminé, nous avons commandé des pièces et nous nous sommes préparés à l'assemblage.",
-			dec: "C'est en décembre que nous avons commencé à construire. Les premiers prototypes que nous avons réalisés étaient grossiers, mais après quelques essais et erreurs, nous sommes parvenus à une bonne conception de la base."
+			dec: "C'est en décembre que nous avons commencé à construire. Les premiers prototypes que nous avons réalisés étaient grossiers, mais après quelques essais et erreurs, nous sommes parvenus à une bonne conception de la base.",
+			jan: "C'est à ce moment-là que nous avons commencé à travailler sur les mécanismes de ramassage des billes. Nous avons réalisé trois prototypes avant de trouver le bon. Entre-temps, nous avons également travaillé sur les mécanismes de tir."
 		}
 	},
-	experience: {
-		title1 : "L'expérience des étudiants",
-		xpgallerylink: "Voir la Galerie", 
-		stxp1: "La compétition CRC est vraiment l'une des plus grandes opportunités pour notre équipe. L'année dernière, c'était la première fois que nous revenions au CRC et, sans expérience, nous avons tout de même réussi à obtenir un excellent classement, à tisser des liens avec d'autres écoles et à acquérir davantage de connaissances sur ce qu'il faut préparer et comment se préparer pour l'année à venir. ",
-		stxp2: "L'année dernière, nous avons eu beaucoup de mal à nous organiser, alors cette année, nous en avons tenu compte. Nous avons créé des équipes et des sous-équipes afin d'être plus efficaces. Cependant, nous avons accepté trop de personnes dans l'équipe et nous n'avons pas pu suivre qui était où et qui faisait quoi. Il y a donc eu des conflits pour déterminer qui devait aller où en fonction de ses compétences, mais nous nous en sommes sortis ensemble. Aujourd'hui, nous avons réussi à stabiliser le flux et tout se passe bien. ",
-		stxp3: "L'une des nombreuses valeurs d'apprentissage a été la compréhension du rôle de leader. Nous avons tous coopéré pour déléguer des tâches et maintenir l'ordre dans nos équipes. ",
-		stxp4: "La partie la plus intéressante de l'équipe de robotique est qu'elle est principalement composée de diplômés, ce qui nous permet de créer de nombreux souvenirs inoubliables. La course aux collations dans la salle des robots, la vieille radio à moitié cassée qui tient compagnie à l'équipe du kiosque pendant qu'elle peint, et l'équipe de programmation qui passe 'le meilleur moment de sa vie' à déboguer son travail.",
-		stxp5: "De nombreux modèles de ballons ont été utilisés pour jouer à l'attrapeur agressif et de nombreux cheveux ont été arrachés à cause du stress. Sans oublier de mentionner les énormes poches sous les yeux de la plupart des gens. Beaucoup de temps a été sacrifié et de nombreuses heures d'étude se sont transformées en séances de construction de robots ou de peinture de kiosques, ce qui s'est traduit par de mauvaises notes aux examens. De plus, j'ai passé d'innombrables jours à l'extérieur avec nos amis, mais j'ai continué à travailler sur les éléments à soumettre PENDANT NOTRE PAUSE. Les larmes n'ont pas seulement coulé, elles ont jailli. "
+	roster: {
+		leaders: "Meneurs",
+		roles: {
+			art: "L'Art Digital",
+			captain: "Capitaine",
+			kiosk: "Kiosque",
+			media: "Médias / Production",
+			pilot: "Pilote",
+			programming: "Programmation",
+			robot_building: "Construction de robots",
+			robot_design: "et Conception",
+			secondary: "Secondaire",
+			teacher: "Enseignant",
+			website: "Site Web"
+		},
+		team: "Équipe",
+		title: "LHA Robotics 2024 Roster"
 	},
 	game: {
 		title1: "La Compétition",
 		title2: "Le Jeu",
-		game1:  "Le concours annuel de robotique du CRC permet aux élèves de différentes écoles et de différents âges de démontrer leurs compétences de manière créative et innovante.",
-		game2:  "Tout d'abord, nous sommes chargés de concevoir et de construire un robot qui participera au jeu le jour de la compétition. Deuxièmement, construire et décorer un kiosque sur le thème choisi par l'équipe. Troisièmement, nous avons filmé et monté une vidéo pour créer une narration en corrélation avec le thème choisi et expliquer le jeu et d'autres informations importantes. Enfin et surtout, la programmation. Nous devons développer un site web🎀 et programmer le robot. De plus, si des mini-jeux sont inclus dans le kiosque, c'est généralement l'équipe de programmation qui s'en charge.",
-		game3:  "Tous ces éléments doivent être accomplis avant la soumission ; la seule exception est la compétition. Chaque année, un nouveau jeu est fourni à toutes les équipes le jour du coup d'envoi, et le jour de la compétition, chaque équipe est tenue de respecter les règles et règlements de ce jeu particulier.",
-		game4:  "La compétition de cette année s'appelle Kryptik. Elle se déroulera à l'école secondaire Curé-Antoine-Labelle, 216 Boul Marc-Aurèle-Fortin, Laval, QC, H7L 1Z5, du 11 au 13 avril.",
-		game5:  "L'objectif est simple: obtenir le plus grand nombre de points possible pour votre équipe. Il y a une équipe bleue et une équipe jaune, chacune avec deux robots qui s'échangent au cours de la compétition. Le jeu se déroule sur le terrain de jeu représenté ci-dessous:",
-		game6:  "Les robots commencent dans la zone désignée par la couleur attribuée et sont contrôlés par les pilotes et dirigés par les copilotes de l'équipe.  Ils doivent rester dans leur camp ou dans la zone neutre indiquée par la zone rouge au milieu, où les pièces de jeu seront placées au début. Les équipes doivent rester dans la zone rouge après la récupération des balles au début du jeu.",
-		game7:  "De chaque côté du terrain de jeu, il y aura 2 des cibles suivantes:",
-		game8:  "Il y aura une cible haute à l'arrière de chaque côté et une cible courte du côté de la couleur de chaque équipe. Le but est de faire entrer les pièces du jeu dans les cibles ou sur le multijoueur et d'obtenir le plus de points possible pour votre équipe.",
-		game9:  "Il y a également trois multiplicateurs des deux côtés. Ces poteaux de différentes tailles sont disséminés sur le terrain, et selon le nombre de pièces de jeu que vous placez sur le poteau, votre score est multiplié.",
+		game1: "Le concours annuel de robotique du CRC permet aux élèves de différentes écoles et de différents âges de démontrer leurs compétences de manière créative et innovante.",
+		game2: "Tout d'abord, nous sommes chargés de concevoir et de construire un robot qui participera au jeu le jour de la compétition. Deuxièmement, construire et décorer un kiosque sur le thème choisi par l'équipe. Troisièmement, nous avons filmé et monté une vidéo pour créer une narration en corrélation avec le thème choisi et expliquer le jeu et d'autres informations importantes. Enfin et surtout, la programmation. Nous devons développer un site web🎀 et programmer le robot. De plus, si des mini-jeux sont inclus dans le kiosque, c'est généralement l'équipe de programmation qui s'en charge.",
+		game3: "Tous ces éléments doivent être accomplis avant la soumission ; la seule exception est la compétition. Chaque année, un nouveau jeu est fourni à toutes les équipes le jour du coup d'envoi, et le jour de la compétition, chaque équipe est tenue de respecter les règles et règlements de ce jeu particulier.",
+		game4: "La compétition de cette année s'appelle Kryptik. Elle se déroulera à l'école secondaire Curé-Antoine-Labelle, 216 Boul Marc-Aurèle-Fortin, Laval, QC, H7L 1Z5, du 11 au 13 avril.",
+		game5: "L'objectif est simple: obtenir le plus grand nombre de points possible pour votre équipe. Il y a une équipe bleue et une équipe jaune, chacune avec deux robots qui s'échangent au cours de la compétition. Le jeu se déroule sur le terrain de jeu représenté ci-dessous:",
+		game6: "Les robots commencent dans la zone désignée par la couleur attribuée et sont contrôlés par les pilotes et dirigés par les copilotes de l'équipe.  Ils doivent rester dans leur camp ou dans la zone neutre indiquée par la zone rouge au milieu, où les pièces de jeu seront placées au début. Les équipes doivent rester dans la zone rouge après la récupération des balles au début du jeu.",
+		game7: "De chaque côté du terrain de jeu, il y aura 2 des cibles suivantes:",
+		game8: "Il y aura une cible haute à l'arrière de chaque côté et une cible courte du côté de la couleur de chaque équipe. Le but est de faire entrer les pièces du jeu dans les cibles ou sur le multijoueur et d'obtenir le plus de points possible pour votre équipe.",
+		game9: "Il y a également trois multiplicateurs des deux côtés. Ces poteaux de différentes tailles sont disséminés sur le terrain, et selon le nombre de pièces de jeu que vous placez sur le poteau, votre score est multiplié.",
 		game10: "Toutes les équipes jouant doivent être identifiées par leur couleur et leur numéro avant les matchs.",
 		game11: "L'arbitre a toute autorité et peut porter n'importe quel jugement sur le jeu. Par exemple, si une pièce d'un robot tombe et empêche les autres joueurs de continuer, si un robot endommage le terrain de jeu ou une pièce de jeu, empêcher les robots de violer l'espace aérien sur les bords du terrain de jeu et s'assurer que le décompte des points est exact.",
 	}
