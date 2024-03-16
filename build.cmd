bd4fcd6e192eadedd1d93687effd1aad1b9f4519
Julian Sarli <sarlij485@gmail.com>
2024-03-16 00:28:03 -0400

    this command is now useless

--- build.cmd
+++ build.cmd
@@ -4,8 +4,6 @@ cd serverScripts
 
 node build.js
 
-del /Q "dist\locales\*.md"
-
 copy ..\robots.txt dist
 
 xcopy dist ..\dist\ /E /Y && rmdir /S /Q dist
--- build.sh
+++ build.sh
@@ -4,8 +4,6 @@ cd serverScripts || exit
 
 node build.js
 
-rm -f dist/locales/*.md
-
 cp ../robots.txt dist
 
 cp -rf dist/* ../dist/ && rm -rf dist
